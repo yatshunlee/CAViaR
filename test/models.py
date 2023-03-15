2683b50474ba30bc91025dfcf0be104deb5f12df
yatshunlee <yatshunlee@gmail.com>
2023-03-15 04:02:47 +0000

    completed CAViaR Frequentist Approach

--- test/models.py
+++ test/models.py
@@ -91,3 +91,86 @@ class GarchModel:
         returns = np.array(returns)
         self.res = minimize(self.neg_log_likelihood, params, args=(returns), bounds=bounds)
         return self.res
+
+
+class CaviarModel:
+    def __init__(self, quantile, model='asymmetric', tol=1e-8):
+        if quantile > 0 and quantile < 1:
+            self.quantile = quantile
+        else:
+            raise ValueError('Quantile must be within 0, 1 exclusively.')
+        
+        if model in ['adaptive', 'symmetric', 'asymmetric']:
+            self.model = model
+        else:
+            raise ValueError('Model must be one of {"adaptive", "symmetric", "asymmetric"}')
+            
+        self.tol = tol
+        
+    def symmetric_abs_val(self, returns, params):
+        b1, b2, b3 = params
+        sigmas = np.zeros_like(returns)
+        sigmas[0] = np.std(returns) # im not sure
+        for t in range(1, len(sigmas)):
+            sigmas[t] = b1 + b2 * sigmas[t-1] + b3 * abs(returns[t-1])
+        return sigmas
+
+    def asymmetric_slope(self, returns, params):
+        b1, b2, b3, b4 = params
+        sigmas = np.zeros_like(returns)
+        sigmas[0] = np.std(returns) # im not sure
+        for t in range(1, len(sigmas)):
+            sigmas[t] = b1 + b2 * sigmas[t-1] + max(b3 * returns[t-1], 0) + b4 * min(b3 * returns[t-1], 0)
+        return sigmas
+
+    def IGARCH(self, returns, params):
+        """one thing: igarch: b2 + b3 == 1."""
+        b1, b2, b3 = params
+        sigmas = np.zeros_like(returns)
+        sigmas[0] = np.std(returns) # im not sure
+        for t in range(1, len(sigmas)):
+            sigmas[t] = (b1 + b2 * sigmas[t-1]**2 + b3 * returns[t-1]**2)**0.5
+        return sigmas
+
+    def neg_log_likelihood(self, params, returns, caviar_func):
+        """return negative log likelihood"""
+        T = len(returns)
+        
+        tau = params[0]
+        betas = params[1:]
+
+        sigmas = caviar_func(returns, betas)
+
+        llh = (1 - T) * np.log(tau)
+        for t in range(1, T):
+            llh -= 1 / tau * max((self.quantile - 1) * (returns[t] - sigmas[t]),
+                                 self.quantile * (returns[t] - sigmas[t]))
+
+        return -llh
+    
+    def initiate_params(self):
+        """
+        generate the initial estimate of tau and betas
+        
+        for the constraints:
+        The first and third of these respond symmetrically to past returns,
+        whereas the second allows the response to positive and
+        negative returns to be different. All three are mean-reverting
+        in the sense that the coefficient on the lagged VaR is not constrained
+        to be 1.
+        """
+        num_params = 3 if self.model in ['symmetric', 'igarch'] else \
+                     4 if self.model == 'asymmetric' else 1
+        bounds = [(1e-4, None), (0, None)] + [(-1, 1) for i in range(num_params - 1)]
+#         bounds = [(1e-4, None), (None, None)] + [(-1, 1) for i in range(num_params - 1)]
+        # bounds = [(1e-4, None), (None, None)] + [(0, 1) for i in range(num_params - 1)]
+        # bounds = [(1e-4, None), (None, None)] + [(None, None) for i in range(num_params - 1)]
+        return np.random.uniform(0, 1, 1 + num_params), bounds
+    
+    def fit(self, returns):
+        """return scipy optimize result"""
+        params, bounds = self.initiate_params()
+        returns = np.array(returns)
+        self.res = minimize(self.neg_log_likelihood, params,
+                            args=(returns, self.asymmetric_slope), bounds=bounds)
+        return self.res
