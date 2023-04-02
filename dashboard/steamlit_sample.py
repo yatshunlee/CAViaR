be0d28dab7242f1a14e849688a5b8a888cd1d34d
angusauyeung <94424856+angusauyeung@users.noreply.github.com>
2023-04-03 01:23:20 +0800

    Add files via upload

--- dashboard/steamlit_sample.py
+++ dashboard/steamlit_sample.py
@@ -15,23 +15,98 @@ from stocknews import StockNews # pip install stocknews
 import plotly.express as px #!pip install pip install plotly-express
 from alpha_vantage.fundamentaldata import FundamentalData #pip install alpha-vantage
 
+from datetime import datetime, timedelta 
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from arch import arch_model
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import vartests
+
+# call the package from _test.py
+from var_tests import binomial_test, kupiec_pof_test, christoffersen_test, traffic_light_test
+from caviar import CaviarModel
 
 #side bar
 
 st.title('Stock Dashboard')
 ticker = st.sidebar.text_input('Ticker')
-start_date = st.sidebar.date_input('Start date')
-end_date = st.sidebar.date_input('End date')
+#start_date = st.sidebar.date_input('Start date')
+#end_date = st.sidebar.date_input('End date')
+
+start_date = datetime(2015,1,1)
+end_date = datetime.today()
+obs_day = end_date - timedelta(days=252*2)
+forecast_day = obs_day + timedelta(days=1)
+data = yf.download(ticker, start=start_date, end=end_date)
+returns = data['Adj Close'].pct_change().dropna()
+
+option = st.selectbox("CARViar model",("adaptive", "symmetric", "asymmetric", "igarch"))
+method = ['mle', 'numeric']
+quantile = 0.05
+
+caviar = CaviarModel(quantile, model=option, method=method[0])
+caviar.fit(returns.loc[:obs_day])
+c_VaRs = caviar.predict(returns.loc[forecast_day:], caviar.VaR0_out)
+current_VaRs = round(c_VaRs[-1]*100,4)
+previous_VaRs = round(c_VaRs[-2]*100,4)
+st.metric(label="Current_VaRs", value=current_VaRs, delta= previous_VaRs - current_VaRs, delta_color='inverse')
 
 #download and plot chart
-#to do: change the end date to 1 year time frame
 
-data = yf.download(ticker, start=start_date, end=end_date)
+#st.metric("VaR:", VaR_indicator[:-1])
 fig = px.line(data, x=data.index, y=data['Adj Close'], title=ticker)
+#fig.add_scatter(x=data.index, y=c_VaRs[:-1], title="5% quantile")
 st.plotly_chart(fig)
 
-#display inf
+#GARCH model
+result = []
+for i in range(1,3):
+  for j in range(1,3):
+    am = arch_model(returns*100, vol='Garch', mean="Zero", p=i, q=j)
+    res = am.fit(disp='off', last_obs=obs_day)
+    result.append({'mod':res, 'aic':res.aic})
 
+result = sorted(result, key=lambda x: x['aic'])
+res = result[0]['mod']
+
+forecasts = res.forecast(start = forecast_day, reindex=False)
+cond_mean = forecasts.mean
+cond_var = forecasts.variance
+q = am.distribution.ppf([0.01, 0.05])
+value_at_risk = np.sqrt(cond_var).values*q[None,:]
+value_at_risk = pd.DataFrame(value_at_risk, columns=['1%','5%'],index=cond_var.index)
+VaRs = value_at_risk['5%']
+
+#run test to ensure model significant
+b = binomial_test(returns.loc[forecast_day:], VaRs, quantile)
+t, _, _ = traffic_light_test(returns.loc[forecast_day:], VaRs, quantile, num_obs=250, baseline=3)
+k = kupiec_pof_test(returns.loc[forecast_day:], VaRs, quantile)
+c = christoffersen_test(returns.loc[forecast_day:], VaRs)
+
+st.header("Test for GARCH")
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("binomial", b)
+col2.metric("traffic_light", t)
+col3.metric("kupiec", k)
+col4.metric("christoffersen", c)
+
+#run test to ensure model significant
+bi = binomial_test(returns.loc[forecast_day:], c_VaRs[:-1], caviar.quantile)
+dq = caviar.dq_test(returns.loc[forecast_day:], 'out')
+ku = kupiec_pof_test(returns.loc[forecast_day:], c_VaRs[:-1], caviar.quantile)
+ch = christoffersen_test(returns.loc[forecast_day:], c_VaRs[:-1])
+
+st.header("Test for CARViaR")
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("binomial", bi)
+col2.metric("DQ test", dq)
+col3.metric("kupiec", ku)
+col4.metric("christoffersen", ch)
+
+#select CAViaR model by p-value
+#c_beta_p = caviar.beta_pvals
+
+#display inf
 pricing_data, fundamental_data, news = st.tabs(["Price", "Fundamental Data", "Top 10 news"])
 trading_date = 252
 
@@ -63,7 +138,7 @@ with news:
     st.write(f'News Sentiment {news_sentiment}')
 
 with fundamental_data:
-  key = "43OTW409OLX93FWD"  #get a key here: https://www.alphavantage.co/support/#api-key
+  key = ""  #get a key here: https://www.alphavantage.co/support/#api-key
   fd = FundamentalData(key, output_format = "pandas")
   st.subheader("Balance Sheet")
   balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
