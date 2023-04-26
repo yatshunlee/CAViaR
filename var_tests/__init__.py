fdb0802aff2aa890dbf78fdd80e7040df7a7bf9a
Jasper Lee <69416199+yatshunlee@users.noreply.github.com>
2023-04-26 14:46:23 +0800

    Update __init__.py

--- var_tests/__init__.py
+++ var_tests/__init__.py
@@ -1,4 +1,4 @@
-from ._test import binomial_test, kupiec_pof_test, christoffersen_test, traffic_light_test, hit_rate
+from ._test import binomial_test, kupiec_pof_test, christoffersen_test, traffic_light_test, hit_rate, dq_test
 
 __all__ = [
     'binomial_test',
